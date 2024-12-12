dcd3211b22912b78ccecd20d6207be6d3252d4cc
toberdas <tobiasijland@gmail.com>
2024-12-12 21:05:23 +0100

    fix for capitals

--- tools/capitals/capitals.js
+++ tools/capitals/capitals.js
@@ -5,7 +5,7 @@ function onTranslateClick(){
     var url = window.location.href;
     let style = document.querySelector('input[name="capitals-style"]:checked').value;
     let newurl = url.replace("html/capitals", "tools/capitals/pngfiles/" + style);
-    newurl = url.replace("html/capitals.html", "tools/capitals/pngfiles/" + style);
+    newurl = newurl.replace("html/capitals.html", "tools/capitals/pngfiles/" + style);
     loadImages(newurl)
 
 }
